54028e299e79abe6960b8bd4390c9ea48ec91a2a
yarrobong <oneboardsholi@gmail.com>
2023-10-26 10:30:30 +0500

    Добавьте файлы проекта.

---
+++ PuntuationHomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите строку и мы посчитаем сколько в знаков пунктуации");
+string str = Console.ReadLine();
+string pattern = ".,;:!?";
+int i = 0;
+foreach (char item in str)
+{
+    if(pattern.Contains(item))
+    {
+        i++;
+    }
+}
+Console.WriteLine(i);
